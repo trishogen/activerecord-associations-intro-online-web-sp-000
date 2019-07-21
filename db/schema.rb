5651a242ea030914669de70e8618431138ddcfcd
Trish Ogen <patriciaogen@gmail.com>
2019-07-21 13:45:43 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-  end
-
-  create_table "songs", force: :cascade do |t|
-    t.string "name"
-    t.integer "artisit_id"
-    t.integer "genre_id"
   end
 
 end
